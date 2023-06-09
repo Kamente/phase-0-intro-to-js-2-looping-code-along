8498bf8512ea9aebf6ce58a8616b4f4d12490226
Kamente <justinm48000@gmail.com>
2023-06-09 17:43:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+
+
+const names = ["Justin"];
+const messages = writeCards(names);
+function writeCards(names) {
+    const thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+  
+    return thankYouMessages;
+  }
+  
+
+  console.log(messages);
+  
+
+
+  function countDown(number1){
+    for (let i = number1; i>=0; i--){
+        console.log(i);
+    }
+  }
+
+  countDown(number1);
+
+
